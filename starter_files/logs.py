1aa9fe598e200ba29c0393efff28dfafe59eee88
Panth-Shah <panth5shah@gmail.com>
2021-01-19 07:06:41 +0000

    [Udacity Project 2] - Pipeline REST endpoint

--- starter_files/logs.py
+++ starter_files/logs.py
@@ -5,10 +5,15 @@ from azureml.core.webservice import Webservice
 ws = Workspace.from_config()
 
 # Set with the deployment name
-name = ""
+name = "autml-deploy"
 
 # load existing web service
 service = Webservice(name=name, workspace=ws)
+print(service)
+
+# Enable Application Insight
+service.update(enable_app_insights=True)
+
 logs = service.get_logs()
 
 for line in logs.split('\n'):
